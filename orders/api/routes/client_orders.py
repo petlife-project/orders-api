192bfecde5938470d008b9e5ebdb0d25e66e82bf
erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>
2020-08-30 15:19:25 -0300

    Adds the two routes available for this API
    Signed-off-by: erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>

---
+++ orders/api/routes/client_orders.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource
+
+
+class ClientOrders(Resource):
+    """ For a client to place/cancel orders and get their current status."""
+
+    @staticmethod
+    def get():
+        pass
+
+    @staticmethod
+    def post():
+        pass
+
+    @staticmethod
+    def put():
+        pass
