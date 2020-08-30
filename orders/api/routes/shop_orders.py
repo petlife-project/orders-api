e7f81addd55942ccf9efa35516661c8cf47921a1
erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>
2020-08-30 15:19:25 -0300

    Adds the two routes available for this API
    Signed-off-by: erossi-impacta <eric.rossi@aluno.faculdadeimpacta.com.br>

---
+++ orders/api/routes/shop_orders.py
@@ -0,0 +1,13 @@
+from flask_restful import Resource
+
+
+class ShopOrders(Resource):
+    """ For a shop to accept/reject orders and get their current status."""
+
+    @staticmethod
+    def get():
+        pass
+
+    @staticmethod
+    def put():
+        pass
--- orders/app.py
+++ orders/app.py
@@ -6,12 +6,18 @@ from flask_restful import Api
 from cheroot.wsgi import PathInfoDispatcher
 from cheroot.wsgi import Server as WSGIServer
 
+from orders.api.routes.client_orders import ClientOrders
+from orders.api.routes.shop_orders import ShopOrders
+
 APP = Flask(__name__)
 API = Api(APP)
 PORT = int(os.getenv('PORT', '8080'))
 
 DISPATCHER = PathInfoDispatcher({'/': APP})
 SERVER = WSGIServer(('0.0.0.0', PORT), DISPATCHER)
+
+API.add_resource(ClientOrders, '/client_orders')
+API.add_resource(ShopOrders, '/shop_orders')
 
 if __name__ == '__main__':
     SERVER.safe_start()
